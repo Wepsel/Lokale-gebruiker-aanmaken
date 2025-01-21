2a6bf229a34c71b264fd353dd604292e37fadd98
Wessel <129429254+Wepsel@users.noreply.github.com>
2025-01-21 12:53:11 +0100

    Gebruiker aanmaken zonder wachtwoord

--- Lokaal account aanmaken.ps1
+++ Lokaal account aanmaken.ps1
@@ -5,24 +5,23 @@ Start-Transcript -Path $LogFilePath -Append
 try {
     Write-Host "Script gestart op $(Get-Date)"
 
-    # Definieer de gebruikersnaam en wachtwoord
-    $LocalUsername = "Beheerder"
-    $TempPassword = "P@ssword123!"
+    # Definieer de gebruikersnaam
+    $LocalUsername = "Gebruiker"
 
     # Controleer of de gebruiker al bestaat
     $UserExists = @(Get-WmiObject -Class Win32_UserAccount -Filter "Name='$LocalUsername'").Count -gt 0
     if (-not $UserExists) {
         Write-Host "Gebruiker $LocalUsername wordt aangemaakt."
 
-        # Maak het lokale account aan
-        net user $LocalUsername $TempPassword /add
+        # Maak het lokale account aan zonder wachtwoord
+        net user $LocalUsername /add
         Write-Host "Gebruiker $LocalUsername succesvol aangemaakt."
     } else {
         Write-Host "De gebruiker $LocalUsername bestaat al. Geen verdere actie nodig."
     }
 
     # Voeg de gebruiker toe aan de Administrators-groep
-    net localgroup Administrators Beheerder /add
+    net localgroup Administrators $LocalUsername /add
     Write-Host "Gebruiker $LocalUsername toegevoegd aan de Administrators-groep."
 
 } catch {
